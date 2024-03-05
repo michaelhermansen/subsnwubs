a36fc560ecba69f7834cf5368c804fb7a32dfd32
Michael Hermansen <michael.p.hermansen@gmail.com>
2024-03-05 23:00:01 +0100

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import Container from "@/lib/ui/Container";
-import Image from "next/image";
-import Link from "next/link";
 import { episodes } from "@/data/episodes";
+import Container from "@/lib/ui/Container";
 import EpisodeCard from "./EpisodeCard";
 
 export default function Home() {
+  const sortedEpisodes = episodes.sort((a, b) => b.id - a.id);
+
   return (
     <Container>
       <ul className="space-y-16">
-        {episodes.toReversed().map((episode, i) => (
+        {sortedEpisodes.map((episode, i) => (
           <li key={episode.id}>
             <EpisodeCard episode={episode} priorityImage={i < 2} />
           </li>
